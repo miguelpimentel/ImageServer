6367f0ef901249e3ad0335004322af2b797297d5
Miguel Pimentel <miguelpimentel2013@gmail.com>
2018-05-23 09:06:31 -0300

    Added implementation to GET photos from server: * Position to get PATH from image * Added Handle to URL * Passing image for HTTP Request

--- app/app.go
+++ app/app.go
@@ -1,18 +1,28 @@
 package main
 
 import (
+
+	// Formatting and Setting up
+
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"log"
+
+	// Image Handle
+
+	// Http and request
 	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
 
+	// External frameroks
 	"github.com/julienschmidt/httprouter"
 )
-
-// Model
 
 type Photo struct {
 	ID   string `json:"id"`
@@ -21,7 +31,7 @@ type Photo struct {
 
 var photos []Photo
 
-// HTTP Methods
+// HTTP Handlers
 
 func getPhotos(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
@@ -41,6 +51,8 @@ func indexHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	fmt.Fprintf(w, "This is the RESTful api")
 }
 
+// Main function
+
 func main() {
 
 	photos = append(photos, Photo{ID: "nd327dh39w4", Name: "photo324.jpg"})
@@ -51,6 +63,11 @@ func main() {
 	router.GET("/", indexHandler)       // GET
 	router.GET("/photos", getPhotos)    // GET
 	router.POST("/photos", createPhoto) // POST
+	router.GET("/image", handler)       // GET image from server PATH
+
+	// Working with files
+
+	router.ServeFiles("/src/*filepath", http.Dir("/src"))
 
 	// Trigger server
 	env_config()
@@ -68,7 +85,66 @@ func env_config() {
 	}
 }
 
-// Test
+// Get Image from PATH
+
+func handler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	img := getImage()
+	writeImage(w, &img)
+}
+
+func getImage() image.Image {
+
+	// Read image from file that already exists
+	existingImageFile, err := os.Open("a.png")
+
+	if err != nil {
+		// Handle error
+	}
+	defer existingImageFile.Close()
+
+	// Calling the generic image.Decode() will tell give us the data
+	// and type of image it is as a string. We expect "png"
+	imageData, imageType, err := image.Decode(existingImageFile)
+	if err != nil {
+	}
+
+	fmt.Println(imageData)
+	fmt.Println(imageType)
+
+	// We only need this because we already read from the file
+	// We have to reset the file pointer back to beginning
+	existingImageFile.Seek(0, 0)
+
+	// Alternatively, since we know it is a png already
+	// we can call png.Decode() directly
+	loadedImage, err := png.Decode(existingImageFile)
+	if err != nil {
+		// Handler error
+	}
+
+	fmt.Println(loadedImage)
+	return loadedImage
+}
+
+func writeImage(w http.ResponseWriter, img *image.Image) {
+
+	buffer := new(bytes.Buffer) // Define a buffer to store an image to requests
+
+	// Error Handler
+	if err := jpeg.Encode(buffer, *img, nil); err != nil {
+		log.Println("unable to encode image.")
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")                        // Header to set content type an image
+	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes()))) // Header for file size
+
+	// Error Handler
+	if _, err := w.Write(buffer.Bytes()); err != nil {
+		log.Println("unable to write image.")
+	}
+}
+
+// Testing
 
 func return10() int {
 	return 10
