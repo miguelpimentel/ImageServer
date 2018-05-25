e66d443aca2b6b03a9998e00852f288d07c8c704
Alexandre Miguel <aleronupe@gmail.com>
2018-05-25 16:18:08 -0300

    Added tests  to function PostImage
    Co-authored-by: Miguel Pimentel <miguelpimentel2013@gmail.com>

--- app/app_test.go
+++ app/app_test.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"os"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -18,13 +19,20 @@ func TestReturn10(t *testing.T) {
 }
 
 func testJsonResponse(t *testing.T) {
+	rr := httptest.NewRecorder()
+	jsonResponse(rr, http.StatusOK, "The test has passed")
+	//testMessage := "The test has passed"
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Wrong message")
+	}
 
 }
 
-func TestHandler(t *testing.T) {
+func TestGetImage(t *testing.T) {
 
 	router := httprouter.New()
-	router.GET("/images", handler)
+	router.GET("/images", getImage)
 
 	req, _ := http.NewRequest("GET", "/images", nil)
 	rr := httptest.NewRecorder()
@@ -34,3 +42,60 @@ func TestHandler(t *testing.T) {
 		t.Errorf("Wrong status")
 	}
 }
+
+func TestRedirectPostImage(t *testing.T) {
+	testRouter := httprouter.New()
+	testRouter.POST("/upload", postImage)
+
+	req, _ := http.NewRequest("GET", "/upload", nil)
+	rr := httptest.NewRecorder()
+	t.Log(rr.Code)
+
+	testRouter.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("Wrong status")
+	}
+}
+
+func TestPostImageWithoutImage(t *testing.T) {
+
+	router := httprouter.New()
+	router.POST("/upload", postImage)
+
+	req, _ := http.NewRequest("POST", "/upload", nil)
+	rr := httptest.NewRecorder()
+	t.Log(rr.Code)
+
+	router.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Wrong status")
+	}
+}
+
+
+
+func TestPostImageWithImage(t *testing.T) {
+
+	router := httprouter.New()
+	router.POST("/upload", postImage)
+
+	image, _ := os.Open("a.png")
+	req, _ := http.NewRequest("POST", "/upload", image)
+	rr := httptest.NewRecorder()
+	t.Log(rr.Code)
+
+	router.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Wrong status")
+	}
+
+
+}
+
+func testSaveFile(t *testing.T) {
+
+	
+	
+}
+
+
